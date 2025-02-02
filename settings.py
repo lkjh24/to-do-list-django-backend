25ae415f6e1500758ccce0e0546bebe9c5fd93c3
lkjh24 <lkjh24745@gmail.com>
2025-02-02 03:35:46 -0600

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_db',
+        'USER': 'root',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
